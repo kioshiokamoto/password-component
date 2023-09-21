075cfcf9327d351ed01b7c7c34ee2ea2a79505b7
Kioshi Okamoto <kjor29@gmail.com>
2023-09-21 01:02:01 -0500

    Updates

--- assets/preview-b69f5793.js
+++
@@ -1,2 +0,0 @@
-import{_ as t}from"./iframe-426cc539.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-3PUGWF3O-8c9ba705.js").then(e=>e.D),["./DocsRenderer-3PUGWF3O-8c9ba705.js","./iframe-426cc539.js","./index-c013ead5.js","./_commonjsHelpers-725317a4.js","./_getPrototype-dd8f0158.js","./index-d37d4223.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
-//# sourceMappingURL=preview-b69f5793.js.map
